b0be9e479ff73166a319cf49d911664af3f8751c
ccarlossalazar <cdsalazar2@gmail.com>
2025-04-07 15:22:24 -0700

    mend

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -3,10 +3,10 @@
 const Footer = () => {
     return (
         <>
-        <footer className="text-center bg-green-800 text-white font-semibold p-3">
-    <h1 className="p-3">(650) 560-9370 | <a href="westcoastjanitorial.com">westcoastjanitorial.com</a></h1>
-    <h1 className="p-3">Located in Half Moon Bay, California. We also serve all cities in San Mateo County. Licensed in California (#02402155)</h1>
-    <h1>Copyright © 2024. All Rights Reserved Web Design by West Coast Janitorial Services</h1>
+        <footer className="text-center bg-green-800 text-white font-semibold p-4 text-sm">
+    <h1 className="p-2">(650) 560-9370 | <a href="westcoastjanitorial.com">westcoastjanitorial.com</a></h1>
+    <h1 className="p-2">Located in Half Moon Bay, California. We also serve all cities in San Mateo County. Licensed in California (#02402155)</h1>
+    <h1 className="pb-4">Copyright © 2024. All Rights Reserved Web Design by West Coast Janitorial Services</h1>
         </footer>
         </>
     )
