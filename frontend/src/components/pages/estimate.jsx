a2ec3be14bff99dafc9350c6c63976085fc7b408
ccarlossalazar <cdsalazar2@gmail.com>
2025-04-07 17:05:56 -0700

    mend

--- frontend/src/components/pages/estimate.jsx
+++ frontend/src/components/pages/estimate.jsx
@@ -27,18 +27,23 @@ const Estimate = () => {
             </div>
             <div>
             <div>
-            <label className="block">Service Address*</label>
+            <label className="block">Phone Number*</label>
             <input name="phone" placeholder="ex. (123) 456-7899" className="bg-white rounded-lg p-2"></input>            
             </div>
             </div>
             </div>
             <div>
+            <label className="block">Service Address*</label>
             <input name="address" placeholder="ex. 1234 Example Blvd, California" className="bg-white rounded-lg p-2"></input>    
             </div>
             <div>
+            <label className="block">Zip Code*</label>
             <input name="zip" placeholder="ex. 91234" className="bg-white rounded-lg p-2"></input>            
             </div>
+            <div>
+            <label className="block">Apartment/Suite (Optional)</label>
             <input name="apartment" placeholder="Apt. 123, Suite A, Unit 1" className="bg-white rounded-lg p-2"></input>                     
+            </div>
             <div className="mb-6 max-w-3xl w-screen">
             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black text-start">Additional Details:</label>
             <textarea id="message" rows="3" class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border focus:ring-blue-500 focus:border-blue-500 bg-white dark:border-gray-600 dark:placeholder-green dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Anything else you would like us to know?"></textarea>
--- frontend/src/components/pages/login.jsx
+++ frontend/src/components/pages/login.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {User, Lock, ArrowLeft} from 'lucide-react'
 import { Link } from 'react-router-dom'
+import Navbar from '../navbar.jsx'
 
 const Login = () => {
   return (
-<div className=" bg-[#385230] min-w-screen min-h-screen">
-<Link to="/"><button className="flex text-white mx-4 py-4"><ArrowLeft />Back to Home </button></Link>
+<>
+ {/*<Link to="/"><button className="flex text-white mx-4 py-4"><ArrowLeft />Back to Home </button></Link>*/}
+<Navbar />
+<div className=" bg-[#385230] h-screen w-screen flex justify-center items-center">
 <div className="flex justify-center">
-    <div className="flex flex-col bg-white rounded-lg justify-center items-center  max-w-md w-full shadow-2xl p-8">
+    <div className="flex bg-white rounded-lg justify-center items-center max-w-md w-full shadow-2xl p-8">
     <form className="p-5 items-center justify-center">
         <img src="/src/assets/wc.png" className="w-50 h-35 mb-3 ml-5"/>
         <h1 className="text-center">Login</h1>
@@ -31,6 +34,7 @@ const Login = () => {
     </div>
     </div>
 </div>
+</>
   );
 };
 
